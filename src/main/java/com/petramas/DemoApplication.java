2846a4253e563ff2cd48561203909fdf128e736a
Sly Lezama <i202510545@cibertec.edu.pe>
2026-09-05 22:30:57 -0500

    Se agrego PostgreSql y generador de administrador inicial

--- src/main/java/com/petramas/DemoApplication.java
+++ src/main/java/com/petramas/DemoApplication.java
@@ -10,4 +10,21 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@org.springframework.context.annotation.Bean
+	public org.springframework.boot.CommandLineRunner initData(com.petramas.repository.OperarioRepository repo) {
+		return args -> {
+			// Si la base de datos está totalmente vacía, creamos el primer admin
+			if (repo.count() == 0) {
+				com.petramas.model.Operario admin = new com.petramas.model.Operario();
+				admin.setDni("admin"); // Usuario para entrar
+				admin.setClave("12345"); // Contraseña para entrar
+				admin.setNombreApellido("Administrador Maestro");
+				admin.setEspecialidad("Sistemas");
+				admin.setEstado("Administrador"); // Asumo que así diferencias a los admins
+				
+				repo.save(admin);
+				System.out.println("¡Base de datos inicializada! Administrador creado: admin / 12345");
+			}
+		};
+	}
 }
